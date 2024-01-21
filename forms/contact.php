9649384670ae72ee6ecbff903680058dd0f4e586
Usama Nadeem <usamanadeemparacha@gmail.com>
2024-01-21 14:19:33 +0500

    Form

--- forms/contact.php
+++ forms/contact.php
@@ -1,38 +1,34 @@
 <?php
-header("Access-Control-Allow-Origin: *"); // Allow requests from any origin
-header("Access-Control-Allow-Methods: POST"); // Allow only POST requests
-header("Access-Control-Allow-Headers: Content-Type"); // Allow only Content-Type header
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
-// Replace contact@example.com with your real receiving email address
-$receiving_email_address = 'usamanadeemparacha@gmail.com';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Replace contact@example.com with your real receiving email address
+    $receiving_email_address = 'usamanadeemparacha@gmail.com';
 
-if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
-    include($php_email_form);
+    if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+        include($php_email_form);
+    } else {
+        die('Unable to load the "PHP Email Form" Library!');
+    }
+
+    $contact = new PHP_Email_Form;
+    $contact->ajax = true;
+
+    $contact->to = $receiving_email_address;
+    $contact->from_name = $_POST['name'];
+    $contact->from_email = $_POST['email'];
+    $contact->subject = $_POST['subject'];
+
+    $contact->add_message($_POST['name'], 'From');
+    $contact->add_message($_POST['email'], 'Email');
+    $contact->add_message($_POST['message'], 'Message', 10);
+
+    echo $contact->send();
 } else {
-    die('Unable to load the "PHP Email Form" Library!');
+    // Handle other HTTP methods (GET, PUT, DELETE, etc.) if needed
+    header('Allow: POST', true, 405);
+    echo 'Method Not Allowed';
 }
-
-$contact = new PHP_Email_Form;
-$contact->ajax = true;
-
-$contact->to = $receiving_email_address;
-$contact->from_name = $_POST['name'];
-$contact->from_email = $_POST['email'];
-$contact->subject = $_POST['subject'];
-
-// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-/*
-$contact->smtp = array(
-  'host' => 'example.com',
-  'username' => 'example',
-  'password' => 'pass',
-  'port' => '587'
-);
-*/
-
-$contact->add_message($_POST['name'], 'From');
-$contact->add_message($_POST['email'], 'Email');
-$contact->add_message($_POST['message'], 'Message', 10);
-
-echo $contact->send();
 ?>
